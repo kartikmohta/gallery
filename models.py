0829f71d74e924b6f766af37803abe1658b7655e
Kartik Mohta <kartikmohta@gmail.com>
2011-07-09 08:47:05 -0400

    Defining the models

--- models.py
+++ models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    image = models.ImageField(upload_to="photos/%Y/%m/%d")
+    caption = models.CharField(max_length=200, blank=True)
+    date_taken = models.DateTimeField()
+    votes = models.IntegerField(default=0)
+    album = models.ForeignKey('Album', null=True, on_delete=models.SET_NULL)
+    private = models.BooleanField(default=False)
+
+class Album(models.Model):
+    PRIVACY_CHOICES = (
+        ('public', 'Public'),
+        ('link', 'Only with link'),
+        ('private', 'Private'),
+    )
+    name = models.CharField(max_length=100)
+    privacy = models.CharField(max_length=7, default='public', choices=PRIVACY_CHOICES)
+    parent_album = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
+    cover_photo = models.OneToOneField('Photo', related_name='+')
